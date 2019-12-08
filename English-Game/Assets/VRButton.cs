8f071cf2158fd97a3326a1c6b2d74d98e8f7d54e
ikke123123 <45997197+ikke123123@users.noreply.github.com>
2019-12-08 19:33:54 +0100

    Changed my test thing

---
+++ English-Game/Assets/VRButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class VRButton : MonoBehaviour
+{
+    [SerializeField] private readonly UnityEvent buttonPress;
+
+    [Header("Button Settings")]
+    [SerializeField] public readonly float game;
+
+    [HideInInspector] private GameObject button;
+
+    private void Start()
+    {
+        
+    }
+}
