419d36030824c638dcb48f2582e959ce5f48dfc6
ultraPerson <m.j.kanefield@st.hanze.nl>
2019-12-13 22:54:45 +0100

    Max Gits it
    Did I... Did I do it?

---
+++ Sound Executor/Assets/Scripts/BallControl.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+
+    public Vector3 sightline;
+    private Vector3 turning;
+
+    public static GameObject selected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit look = new RaycastHit();
+        sightline = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position, sightline, Color.red);
+        if(Input.anyKeyDown)
+        {
+            
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                turning = new Vector3(0, -3, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                turning = new Vector3(0, 3, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                turning = new Vector3(3, 0, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                turning = new Vector3(-3, 0, 0);
+            }
+        }
+        else
+        {
+            turning = new Vector3(0,0,0);
+        }
+
+        gameObject.transform.Rotate(turning, Space.Self);
+        
+        
+
+        if (Physics.Raycast(transform.position, sightline, 100))
+        {
+            selected = look.collider.gameObject;
+            
+        }
+        
+    }
+}
---
+++ Sound Executor/Assets/Scripts/SoundControl.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundControl : MonoBehaviour
+{
+    //Soundtack, played through the camera
+    public AudioSource soundtrack;
+    //new audio source, selected by player interaction
+    //private GameObject player = GameObject.Find("PlayerBall");
+    private AudioSource newSound;
+    private GameObject selectedObj;
+    
+    private bool playSong;
+
+    public float volLevel = 1;
+    public bool playToggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundtrack = GetComponent<AudioSource>();
+        playSong = true;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        selectedObj = BallControl.selected;
+        newSound = selectedObj.GetComponent<AudioSource>();
+        
+
+        if (playSong == true && playToggle == true)
+        {
+            volLevel = 0;
+
+            
+            playSong = false;
+            playToggle = false;
+        }
+
+        if (playSong == false && playToggle == true)
+        {
+            volLevel = 1;
+            
+            
+            playSong = true;
+            playToggle = false;
+        }
+
+        soundtrack.volume = volLevel;
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Click");
+            AudioInteract();
+        }
+
+
+
+
+    }
+
+    public void AudioInteract()
+    {
+        if (newSound)
+        {
+            newSound.Play();
+        }
+
+    }
+}
