715041744104344f9ec095420c2d98f87097eab6
Aristeus <jornjellema@hotmail.com>
2019-12-23 13:27:50 +0100

    Added bottle again

---
+++ English-Game/Assets/Scripts/BreakScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakScript : MonoBehaviour
+{
+    bool canBreak;
+    private Rigidbody rb;
+    public float breakLimit;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (!canBreak)
+        {
+            if (rb.velocity.magnitude > breakLimit)
+            {
+                canBreak = true;
+            }
+        }
+        //Debug.Log("Bottle " + rb.velocity.magnitude);
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (canBreak)
+        {
+
+            foreach (Transform transyBit in transform)
+            {
+                transyBit.gameObject.GetComponent<Collider>().enabled = true;
+                transyBit.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                transyBit.gameObject.transform.parent = null;
+            }
+        }
+    }
+}
