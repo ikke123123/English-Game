f7ff9f09907a709db3a382bd9029b6d95d96a15f
ikke123123 <45997197+ikke123123@users.noreply.github.com>
2020-01-06 10:46:05 +0100

    Mid-Update

---
+++ English-Game/Assets/Scripts/Data/ObjectCard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum PictureTag { untagged, blue, gray, green, standard, lighter, darker, normal, metallic, transparent };
+
+[CreateAssetMenu(fileName = "New Object Card", menuName = "Custom/Object Card")]
+public class ObjectCard : ScriptableObject
+{
+    //------------------------------------------
+    //             Made By Thomas
+    //------------------------------------------
+    //This script contains data about objects t-
+    //hat are assigned to it.
+
+    [SerializeField] public GameObject prefab;
+    [Header("First Puzzle")]
+    [SerializeField, Tooltip("Can be left empty, is only used for first puzzle. Pick: Blue, Gray, or Green.")] public PictureTag location;
+    [SerializeField, Tooltip("Can be left empty, is only used for first puzzle. Pick: Standard, Lighter, or Darker.")] public PictureTag attribute;
+    [SerializeField, Tooltip("Can be left empty, is only used for first puzzle. Pick: Normal, Metallic, or Transparent.")] public PictureTag activity;
+    [Header("Second Puzzle")]
+    [SerializeField, Tooltip("Can be left empty, is only used for second puzzle.")] public Combinations[] combinations;
+    [HideInInspector] public List<GameObject> assignedGameObject;
+
+    private void OnEnable()
+    {
+        assignedGameObject = new List<GameObject>();
+        //Checks if the PictureTags are viable
+        if (location != PictureTag.untagged)
+        {
+            if (!(location > PictureTag.untagged && location <= PictureTag.green)) Debug.LogWarning("Location is not correct.");
+            if (!(attribute >= PictureTag.standard && attribute <= PictureTag.darker)) Debug.LogWarning("Attribute is not correct.");
+            if (!(activity >= PictureTag.normal && activity <= PictureTag.transparent)) Debug.LogWarning("Activity is not correct.");
+        }
+    }
+}
--- English-Game/Assets/Scripts/ObjectReset.cs
+++ English-Game/Assets/Scripts/ObjectReset.cs
@@ -4,7 +4,25 @@ using UnityEngine;
 
 public class ObjectReset : MonoBehaviour
 {
+<<<<<<< Updated upstream
     [SerializeField] private float minHeight;
+=======
+    //------------------------------------------
+    //             Made By Thomas
+    //------------------------------------------
+    //Script makes sure that the object respawns
+    //when it is below the bounds of the playing
+    //area.
+    //Use this script on objects that are missi-
+    //on critical, and need to be respawned when
+    //they get lost.
+
+    [Header("Settings")]
+    [SerializeField, Tooltip("Object should respawn whenever it gets below the Minimal Height.")] private bool respawnUpdate = false;
+    [SerializeField, Tooltip("Object should respawn whenever it gets destroyed.")] private bool respawnOnDestroy = false;
+    [SerializeField] private float minHeight = -5;
+
+>>>>>>> Stashed changes
     [HideInInspector] private Vector3 startPos;
     [HideInInspector] private Quaternion startRotation;
     [HideInInspector] private Rigidbody rb;
@@ -13,19 +31,56 @@ public class ObjectReset : MonoBehaviour
     private void Start()
     {
         startPos = transform.position;
+<<<<<<< Updated upstream
         if (startPos.y < minHeight) Debug.LogError("Will forever respawn: " + gameObject.name); disable = true;
         startRotation = transform.rotation;
         rb = GetComponent<Rigidbody>();
         if (rb == null) Debug.LogError("Disabled respawning: " + gameObject.name); disable = true;
+=======
+        if (startPos.y < minHeight && respawnUpdate)
+        {
+            Debug.LogWarning("Will forever respawn: " + gameObject.name); 
+            disable = true;
+        }
+  
+        startRotation = transform.rotation;
+        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            Debug.LogWarning("Disabled respawning: " + gameObject.name); 
+            disable = true;
+        }
+>>>>>>> Stashed changes
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        if (disable == false && transform.position.y < minHeight)
+        if (respawnUpdate && disable == false && transform.position.y < minHeight)
         {
             transform.position = startPos;
             transform.rotation = startRotation;
             CodeLibrary.SetVelocity(rb);
         }
     }
+<<<<<<< Updated upstream
+=======
+
+    private void OnDestroy()
+    {
+        if (disable == false && respawnOnDestroy)
+        {
+            GameObject copy = Instantiate(gameObject, startPos, startRotation);
+        }
+    }
+
+    public void ResetPosition()
+    {
+        if (disable == false)
+        {
+            transform.position = startPos;
+            transform.rotation = startRotation;
+            CodeLibrary.SetVelocity(rb);
+        }
+    }
+>>>>>>> Stashed changes
 }
