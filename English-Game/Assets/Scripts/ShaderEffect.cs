195f6530f65dc74139e33eabf3f8d8391e75cc1a
ikke123123 <45997197+ikke123123@users.noreply.github.com>
2020-01-19 23:39:14 +0100

    Tiny Update

---
+++ English-Game/Assets/Scripts/ShaderEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderEffect : MonoBehaviour
+{
+    [SerializeField] private Shader effect;
+
+    private Material material;
+
+    private void Start()
+    {
+        if (TryCreateMaterial() == false)
+        {
+            Debug.LogError("Shader incorrect.");
+        }
+    }
+
+    private void OnEnable()
+    {
+        TryCreateMaterial();
+    }
+
+    private bool TryCreateMaterial()
+    {
+        if (effect == null) return false;
+        if (material == null) material = new Material(effect);
+        return (material != null);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+}
